706c60129549e48df04e9d17318e1fc2eca01122
ArthurThuko <arthurhenriqsfc@gmail.com>
2025-10-28 21:08:10 -0300

    Refatorando: Controller Login refatorada

--- app/src/main/java/alphacontrol/models/LoginService.java
+++ app/src/main/java/alphacontrol/models/LoginService.java
@@ -1,7 +1,10 @@
 package alphacontrol.models;
 
 public class LoginService {
+    private String login = "MerceariaSR";
+    private String senha = "lucas123";
+
     public boolean autenticar(String usuario, String senha) {
-        return usuario.equals("MerceariaSR") && senha.equals("lucas123");
+        return usuario.equals(login) && senha.equals(senha);
     }
 }
